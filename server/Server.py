5c9f754bae0fbfdb5aa388ff440cf76af94bad18
Hao <hao@Haos-MacBook-Pro.local>
2020-12-05 00:01:42 -0500

    client send files to server

---
+++ server/Server.py
@@ -0,0 +1,48 @@
+"""
+Now server can receive upload from client
+"""
+import socket
+import tqdm
+import os
+
+SERVER_HOST = "0.0.0.0"
+SERVER_PORT = 5001
+BUFFER_SIZE = 4096
+SEPARATOR = "<SEPARATOR>"
+
+# create the server socket
+# TCP socket
+s = socket.socket()
+s.bind((SERVER_HOST, SERVER_PORT))
+# enabling our server to accept connections
+# 5 here is the number of unaccepted connections that
+# the system will allow before refusing new connections
+s.listen(5)
+print(f"[*] Listening as {SERVER_HOST}:{SERVER_PORT}")
+client_socket, address = s.accept()
+print(f"[+] {address} is connected.")
+
+# receive the file infos
+received = client_socket.recv(BUFFER_SIZE).decode()
+filename, filesize = received.split(SEPARATOR)
+# remove absolute path if there is
+filename = os.path.basename(filename)
+# convert to integer
+filesize = int(filesize)
+# start receiving the file from the socket
+# and writing to the file stream
+progress = tqdm.tqdm(range(filesize), f"Receiving {filename}", unit="B", unit_scale=True, unit_divisor=1024)
+with open(filename, "wb") as f:
+    for _ in progress:
+        bytes_read = client_socket.recv(BUFFER_SIZE)
+        if not bytes_read:
+            # nothing is received
+            # file transmitting is done
+            break
+        # write to the file the bytes we just received
+        f.write(bytes_read)
+        # update the progress bar
+        progress.update(len(bytes_read))
+
+client_socket.close()
+s.close()
