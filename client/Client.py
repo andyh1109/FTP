0ad629c04bf15f654f746bc2cd2a6608452c343a
weihanLiu <liu.weih@husky.neu.edu>
2020-12-09 17:34:30 -0500

    add control/data socket, add multi-thread, add get/sent/list

--- client/Client.py
+++ client/Client.py
@@ -1,21 +1,30 @@
-"""
-now client can send files to server
-"""
 import socket
 import tqdm
 import os
 import argparse
+import struct
 
 SEPARATOR = "<SEPARATOR>"
 BUFFER_SIZE = 1024 * 4
+# socket type
+CONTROL = 0
+DATA = 1
 
-def send_file(filename, host, port):
+#data request type
+LIST = 1
+GET = 2
+SEND = 3
+EXIT = 4
+
+#status code
+VALID = 1
+INVALID = 0
+
+SECRET_LEN = 12
+
+def send_file(s):
+    filename = input("Enter file name:\n")
     filesize = os.path.getsize(filename)
-    s = socket.socket()
-    print(f"[+] Connecting to {host}:{port}")
-    s.connect((host, port))
-    print("[+] Connected.")
-
     s.send(f"{filename}{SEPARATOR}{filesize}".encode())
 
     # start sending the file
@@ -32,16 +41,111 @@ def send_file(filename, host, port):
             # update the progress bar
             progress.update(len(bytes_read))
 
-    s.close()
+
+
+def get_file(s):
+    filename = input("Enter file name you want to download\n")
+    s.send(struct.pack('Q%ds' % len(filename), len(filename), filename.encode()))
+    # receive the file infos
+    received = s.recv(BUFFER_SIZE).decode()
+    filename, filesize = received.split(SEPARATOR)
+    # remove absolute path if there is
+    filename = os.path.basename(filename)
+    # convert to integer
+    filesize = int(filesize)
+    # start receiving the file from the socket
+    # and writing to the file stream
+    progress = tqdm.tqdm(range(filesize), f"Receiving {filename}", unit="B", unit_scale=True, unit_divisor=1024)
+    with open(filename, "wb") as f:
+        for _ in progress:
+            bytes_read = s.recv(BUFFER_SIZE)
+            if not bytes_read:
+                # nothing is received
+                # file transmitting is done
+                break
+            # write to the file the bytes we just received
+            f.write(bytes_read)
+            # update the progress bar
+            progress.update(len(bytes_read))
+
+
+def list_files(data_socket):
+    while True:
+        bytes_read = data_socket.recv(struct.calcsize('Q'))
+        if not bytes_read:
+            break
+        filenameLen, = struct.unpack('Q',bytes_read)
+        filenameLen = int(filenameLen)
+        filename = data_socket.recv(struct.calcsize('%ds' % filenameLen)).decode()
+        print(filename)
+
+
+
+def connectToServer(host, port):
+    s = socket.socket()
+    print(f"[+] Connecting to {host}:{port}")
+    s.connect((host, port))
+    print("[+] Connected.")
+    return s;
+
+
+def login(control_socket):
+    control_socket.send(struct.pack('b',CONTROL))
+    username = input("Enter username:")
+    password = input("Password:")
+    loginHeader = struct.pack('QQ', len(username),len(password))
+    control_socket.send(loginHeader)
+    loginInfo = struct.pack('%ds%ds' % (len(username),len(password)), username.encode(), password.encode())
+    control_socket.send(loginInfo)
+    validUser, = struct.unpack('b', control_socket.recv(struct.calcsize('b')))
+    if(validUser == VALID):
+        secret = control_socket.recv(struct.calcsize('%ds' % SECRET_LEN)).decode()
+        return secret;
+    else:
+        print('invalid password or username')
+        return -1;
+
+
+def handle_data_transmit_req(secret):
+    select = input("1.List avaliable files in server\n2.Get file from server\n3.Send file to server\n4.Exit\n")
+    if(int(select) == EXIT):
+        return 0
+    data_socket = connectToServer(host, port)
+    data_socket.send(struct.pack('b',DATA))
+    data_socket.send(secret.encode())
+    validSecret, = struct.unpack('b', data_socket.recv(struct.calcsize('b')))
+    if(validSecret == INVALID):
+        print("invalid secret")
+    else:
+        print("secret is correct")
+        if(int(select) == LIST):
+            data_socket.send(struct.pack('b',LIST))
+            list_files(data_socket)
+        elif(int(select) == GET):
+            data_socket.send(struct.pack('b',GET))
+            get_file(data_socket)
+        elif(int(select) == SEND):
+            data_socket.send(struct.pack('b',SEND))
+            send_file(data_socket)
+            data_socket.close()
+        else:
+            print("invalid input, please enter 1, 2, 3, or 4")
+    
+    return 1
 
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser(description="Simple File Sender")
-    parser.add_argument("file", help="File name to send")
     parser.add_argument("host", help="The host/IP address of the receiver")
-    parser.add_argument("-p", "--port", help="Port to use, default is 5001", default=5001)
+    parser.add_argument("-p", "--port", help="Port to use, default is 5001", default=5002)
     args = parser.parse_args()
-    filename = args.file
     host = args.host
     port = args.port
-    send_file(filename, host, port)
+    control_socket = connectToServer(host, port)
+    secret = login(control_socket)
+    keeplooping = 1;
+    if(secret != -1):
+        while keeplooping:
+            keeplooping = handle_data_transmit_req(secret)
+    control_socket.close()
+   
--- server/Server.py
+++ server/Server.py
@@ -1,48 +1,163 @@
-"""
-Now server can receive upload from client
-"""
 import socket
 import tqdm
 import os
+from threading import Thread
+import struct
+
 
 SERVER_HOST = "0.0.0.0"
-SERVER_PORT = 5001
+CONTROL_PORT = 5002
 BUFFER_SIZE = 4096
 SEPARATOR = "<SEPARATOR>"
+RESOURCE_PATH = 'resources/'
+# socket type
+CONTROL = 0
+DATA = 1
 
-# create the server socket
-# TCP socket
-s = socket.socket()
-s.bind((SERVER_HOST, SERVER_PORT))
-# enabling our server to accept connections
-# 5 here is the number of unaccepted connections that
-# the system will allow before refusing new connections
-s.listen(5)
-print(f"[*] Listening as {SERVER_HOST}:{SERVER_PORT}")
-client_socket, address = s.accept()
-print(f"[+] {address} is connected.")
+#data request type
+LIST = 1
+GET = 2
+SEND = 3
 
-# receive the file infos
-received = client_socket.recv(BUFFER_SIZE).decode()
-filename, filesize = received.split(SEPARATOR)
-# remove absolute path if there is
-filename = os.path.basename(filename)
-# convert to integer
-filesize = int(filesize)
-# start receiving the file from the socket
-# and writing to the file stream
-progress = tqdm.tqdm(range(filesize), f"Receiving {filename}", unit="B", unit_scale=True, unit_divisor=1024)
-with open(filename, "wb") as f:
-    for _ in progress:
-        bytes_read = client_socket.recv(BUFFER_SIZE)
-        if not bytes_read:
-            # nothing is received
-            # file transmitting is done
-            break
-        # write to the file the bytes we just received
-        f.write(bytes_read)
-        # update the progress bar
-        progress.update(len(bytes_read))
 
-client_socket.close()
-s.close()
+VALID = 1
+INVALID = 0
+
+SECRET = 'randomsecret'
+SECRET_LEN =12
+
+data_connections = []
+control_connections = []
+VALID_USERS = {}
+
+
+
+# received file from client
+def handle_send_request(client_socket):
+    # receive the file infos
+    received = client_socket.recv(BUFFER_SIZE).decode()
+    filename, filesize = received.split(SEPARATOR)
+    # remove absolute path if there is
+    filename = os.path.basename(filename)
+    filename = os.path.abspath(os.path.join(os.getcwd(),RESOURCE_PATH, filename))
+    # convert to integer
+    filesize = int(filesize)
+    # start receiving the file from the socket
+    # and writing to the file stream
+    progress = tqdm.tqdm(range(filesize), f"Receiving {os.path.basename(filename)}", unit="B", unit_scale=True, unit_divisor=1024)
+    with open(filename, "wb") as f:
+        for _ in progress:
+            bytes_read = client_socket.recv(BUFFER_SIZE)
+            if not bytes_read:
+                # nothing is received
+                # file transmitting is done
+                break
+            # write to the file the bytes we just received
+            f.write(bytes_read)
+            # update the progress bar
+            progress.update(len(bytes_read))
+
+def handle_list_request(client_socket):
+    files = os.listdir(os.path.join(os.getcwd(),RESOURCE_PATH))
+    for f in files:
+        filelen = len(f)
+        client_socket.send(struct.pack('Q%ds' % filelen, filelen, f.encode()))
+
+
+# send file to client
+def handle_get_request(client_socket):
+    filenameLen, = struct.unpack('Q', client_socket.recv(struct.calcsize('Q')))
+    filenameLen = int(filenameLen)
+    filename = client_socket.recv(struct.calcsize('%ds' % filenameLen)).decode()
+    filename = os.path.abspath(os.path.join(os.getcwd(),RESOURCE_PATH, filename))
+
+    filesize = os.path.getsize(filename)
+    client_socket.send(f"{filename}{SEPARATOR}{filesize}".encode())
+
+    # start sending the file
+    progress = tqdm.tqdm(range(filesize), f"Sending {os.path.basename(filename)}", unit="B", unit_scale=True, unit_divisor=1024)
+    with open(filename, "rb") as f:
+        for _ in progress:
+            bytes_read = f.read(BUFFER_SIZE)
+            if not bytes_read:
+                # file transmitting is done
+                break
+    
+            client_socket.sendall(bytes_read)
+            # update the progress bar
+            progress.update(len(bytes_read))
+
+
+#TODO
+def isValidSecret(userSecret):
+    return userSecret == SECRET
+
+
+def handle_data_request(data_socket):
+    userSecret = data_socket.recv(struct.calcsize('%ds' % SECRET_LEN)).decode()
+    if(isValidSecret(userSecret)):
+        data_socket.send(struct.pack('b',VALID))
+        reqType, = struct.unpack('b', data_socket.recv(struct.calcsize('b')))
+        if(reqType == LIST):
+            handle_list_request(client_socket)
+            client_socket.close()
+        elif(reqType == GET):
+            handle_get_request(client_socket)
+            client_socket.close()
+        elif(reqType == SEND):
+            handle_send_request(client_socket)
+    else:
+        data_socket.send(struct.pack('b',INVALID))
+
+
+def handle_login_request(control_socket,valid_users):
+    login_headinfo = client_socket.recv(struct.calcsize('QQ'))
+    usernameLen,pswLen = struct.unpack('QQ', login_headinfo)
+    print(usernameLen, pswLen)
+    username= client_socket.recv(usernameLen).decode()
+    psw = client_socket.recv(pswLen).decode()
+    if(username in valid_users and valid_users[username] == psw):
+        control_socket.send(struct.pack('b',VALID))
+        control_socket.send(SECRET.encode())
+    else:
+        control_socket.send(struct.pack('b',INVALID))
+    
+
+def read_registed_user_to_memory():
+    with open("user.txt") as f:
+        valid_users = dict(x.rstrip().split(None, 1) for x in f)
+    # for i in valid_users:
+    #     print(f"user:{i}, pw:{valid_users[i]}")
+    return valid_users
+
+
+if __name__ == "__main__":
+    # create the server socket
+    # TCP socket
+    socket = socket.socket()
+    socket.bind((SERVER_HOST, CONTROL_PORT))
+    valid_users = read_registed_user_to_memory()
+    # enabling our server to accept connections
+    # 5 here is the number of unaccepted connections that
+    # the system will allow before refusing new connections
+    while True:
+        socket.listen(5)
+        #print(f"[*] Listening as {SERVER_HOST}:{CONTROL_PORT}")
+        client_socket, address = socket.accept()
+        print(f"[+] {address} is connected.")
+        identify_info = client_socket.recv(struct.calcsize('b'))
+        print("received")
+        msgType, = struct.unpack('b', identify_info)
+        if(msgType == CONTROL):
+            t = Thread(target=handle_login_request, args=(client_socket, valid_users)).start();
+            control_connections.append(t)
+        elif(msgType == DATA):
+            t = Thread(target=handle_data_request, args=(client_socket, )).start();
+            data_connections.append(t)
+
+    
+    for i in data_connections:
+        i.join()
+    for i in control_connections:
+        i.join()
+    socket.close()
